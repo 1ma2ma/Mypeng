c2f8eb60446ce3745f25cccdaf594b8ad53c49ad
jiwoo0305 <jiwoo0305@naver.com>
2022-05-02 22:57:30 +0900

    뭔가햇음아닐수도잇음

---
+++ Mypeng/Assets/Rresource/Character/Player.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool wDown;
+    bool jDown;
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+
+        GetInput();
+        Move();
+        Turn();
+
+
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+    }
+
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+
+    }
+}
